8bbf44016a693eaad7373829ac0f2eb69dd30a24
liugaowei999 <187346454@qq.com>
2018-04-20 13:51:10 +0800

    增加描述

--- liugw/src/main/java/com/cttic/liugw/design/decorator/CoffeeSaleTest.java
+++ liugw/src/main/java/com/cttic/liugw/design/decorator/CoffeeSaleTest.java
@@ -9,6 +9,16 @@ import com.cttic.liugw.design.decorator.condiment.Milk;
 import com.cttic.liugw.design.decorator.condiment.Mocha;
 import com.cttic.liugw.design.decorator.condiment.Soy;
 
+/**
+ * 装饰者模式 ： 为现有的类提供新的功能， 不改变接口
+ * 
+ * 与适配器模式的区别： 
+ ******* 适配器模式： 针对新系统接口，为客户提供期望的接口，使类之间能够无碍的交流； 会改变接口以适配客户的要求
+ *       外观模式： 目的是简化现有系统接口的调用，使客户更方便的使用接口； 同时也可以做到客户与具体系统的解耦。
+ * 
+ * @author liugaowei
+ *
+ */
 public class CoffeeSaleTest {
     public static void main(String[] args) {
         Beverage beverage = new Espresso();
