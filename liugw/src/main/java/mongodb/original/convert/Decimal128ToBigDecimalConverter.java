dbe99a260bd87fb677732af1a7ea5e9351e29496
liugaowei999 <187346454@qq.com>
2018-09-21 20:38:35 +0800

    mongodb

---
+++ liugw/src/main/java/mongodb/original/convert/Decimal128ToBigDecimalConverter.java
@@ -0,0 +1,15 @@
+package mongodb.original.convert;
+
+//import org.bson.types.Decimal128;
+//import org.springframework.core.convert.converter.Converter;
+//
+//public class Decimal128ToBigDecimalConverter implements Converter<Decimal128, BigDecimal> {
+//
+//	@Override
+//	public BigDecimal convert(Decimal128 source) {
+//		return source.bigDecimalValue();
+//	}
+//	
+//	
+//
+//}
