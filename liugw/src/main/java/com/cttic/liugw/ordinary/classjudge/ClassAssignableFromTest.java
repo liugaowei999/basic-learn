378fbfc8e7e3523a03e63fc5320a9cd7ee60f53c
gaowei.liu <gaowei.liu@ly.com>
2019-02-12 15:16:24 +0800

    add package

---
+++ liugw/src/main/java/com/cttic/liugw/ordinary/classjudge/ClassAssignableFromTest.java
@@ -0,0 +1,40 @@
+package com.cttic.liugw.ordinary.classjudge;
+
+public class ClassAssignableFromTest {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        A ba = new B();
+        System.out.println("1-------------");
+        System.out.println(A.class.isAssignableFrom(a.getClass()));
+        System.out.println(B.class.isAssignableFrom(b.getClass()));
+        System.out.println(A.class.isAssignableFrom(b.getClass()));
+        System.out.println(B.class.isAssignableFrom(a.getClass()));
+        System.out.println(A.class.isAssignableFrom(ba.getClass()));
+        System.out.println(B.class.isAssignableFrom(ba.getClass()));
+
+        System.out.println("2-------------");
+        System.out.println(a.getClass().isAssignableFrom(A.class));
+        System.out.println(b.getClass().isAssignableFrom(B.class));
+        System.out.println(a.getClass().isAssignableFrom(B.class));
+        System.out.println(b.getClass().isAssignableFrom(A.class));
+        System.out.println(ba.getClass().isAssignableFrom(A.class));
+        System.out.println(ba.getClass().isAssignableFrom(B.class));
+
+        System.out.println("3-------------");
+        System.out.println(Object.class.isAssignableFrom(b.getClass()));
+        System.out.println(Object.class.isAssignableFrom("abc".getClass()));
+
+        System.out.println("4-------------");
+        System.out.println("a".getClass().isAssignableFrom(Object.class));
+        System.out.println("abc".getClass().isAssignableFrom(Object.class));
+    }
+
+
+}
+
+class A {
+}
+
+class B extends A {
+}
