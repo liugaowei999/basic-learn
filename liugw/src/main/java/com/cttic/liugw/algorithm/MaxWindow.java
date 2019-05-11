b029e17d58f50d3603200cad64c1924527589354
gaowei.liu <gaowei.liu@ly.com>
2019-05-11 15:32:04 +0800

    maxWindow

---
+++ liugw/src/main/java/com/cttic/liugw/algorithm/MaxWindow.java
@@ -0,0 +1,57 @@
+package com.cttic.liugw.algorithm;
+
+import java.util.LinkedList;
+
+
+public class MaxWindow {
+    static int[] arr = {4,3,5,4,3,3,6,7};
+
+
+    public static int[] getMaxWindow(int[] array, int widowSize) {
+        int result[] = new int[array.length - widowSize + 1];
+        if( null == array || widowSize < 1 || array.length < widowSize ) {
+            return result;
+        }
+        LinkedList<Integer> qmax = new LinkedList<>();
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            while (!qmax.isEmpty() && array[qmax.peekLast()] <= array[i]) {
+                qmax.pollLast();
+            }
+            qmax.addLast(i);
+            if (qmax.peekFirst() == i - widowSize) {
+                System.out.println("执行弹出， i:" + (i) + ", qmax.peekFirst()=" + qmax.peekFirst());
+                qmax.pollFirst();
+            }
+            if (i >= widowSize - 1) {
+                result[index++] = array[qmax.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + ",");
+        }
+        System.out.println();
+
+        LinkedList<Integer> qmax = new LinkedList<>();
+        qmax.addLast(1);
+        qmax.addLast(2);
+        System.out.println(qmax.peekFirst());
+        System.out.println(qmax);
+        System.out.println(qmax.pollFirst());
+        System.out.println(qmax);
+
+        int[] maxWindow = getMaxWindow(arr, 2);
+        for (int i=0; i<maxWindow.length; i++) {
+            System.out.print(maxWindow[i] + ",");
+        }
+        System.out.println();
+
+    }
+}
